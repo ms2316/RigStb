70c5a4461331eb6bd2878962a4d5d7081c6efe83
Mihails Smolins <mihailssmolins@youview.com>
2016-06-15 13:53:20 +0100

    added get_stb_status and status_record

--- script.py
+++ script.py
@@ -1,23 +1,39 @@
-import urllib2, json
+import urllib2
+import json
 
-data = {}
+#Returns url of the given rig/stb
+def get_url(rig, stb):
+	if rig == 10:
+		return "http://testaut-10-switch-pc-1:10000/stb-" + str(stb)
+	else:
+		return "http://testaut-" + str(rig) + "-switch-pc:10000/stb-" + str(stb)
 
-#gather status of rigs 1 to 9
-for rig in range(1,10):
-	for stb in range(1,8):
-		url = "http://testaut-" + str(rig) + "-switch-pc:10000/stb-" + str(stb)
-		response = urllib2.urlopen(url)
-		jdata = json.load(response)
-		#validity check
-		data["rig" + str(rig) + "stb" + str(stb)] = jdata
+#Gets stb status and throws HTTPError exception
+def get_stb_status(rig, stb):
+	#try:
+	response = urllib2.urlopen( get_url(rig, stb) )
+	jdata = json.load(response)
+	return jdata
+	#except urllib2.HTTPError:
+	#	return "Invalid rig/stb"
 
-#gather status of rig 10
-
-
-
+def status_record():
+	data = {}
+	#gather status of rigs 1 to 10
+	for rig in range(1,11):
+		for stb in range(1,8):
+			try:
+				jdata = get_stb_status(rig,stb)
+				data["rig" + str(rig) + "stb" + str(stb)] = jdata
+			except Exception:
+				#get_stb_status returned with an error
+				pass
+	#flush data to DB
 
 #test
-tmp = data["rig3stb2"]
+status_record()
+
+tmp =  get_stb_status(1,8)
 box = tmp['box']
 print 'version: ' + box['cds_version']
 print "ip: " + box["ip"]
@@ -26,5 +42,3 @@ print "model: " + box["model"]
 print "oem: " + box["oem"]
 print "ruid: " + box["ruid"]
 print "variant: " + box["variant"]
-	
-	
